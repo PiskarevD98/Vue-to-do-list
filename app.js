c471e85344f57549b342fbabb25f2b00045dc2f4
PiskarevD98 <piskar98@mail.ru>
2022-09-15 19:25:16 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+Vue.createApp({
+    // data() {
+    //     return {
+    //     title: 'Я есть грут',
+    //     }
+    // } --> es5
+    data: () => ({
+        Html: '<h1>Планы на сегодня</h1>',
+        title: 'Мои планы',
+        person : {
+            firstName: 'Dima',
+            lastName: 'Pi4kar',
+            age: 24,
+        },
+        items: [],
+        status: true,
+    }),
+    methods: {
+        addItem() {
+            this.items.unshift(this.$refs.myInput.value)
+            this.$refs.myInput.value = ''
+        },
+        remove(i) {
+            this.items.splice(i, 1)
+        },
+    }, 
+    computed: {
+        eventItems() {
+            return this.items
+        },
+        done() {
+            if(this.status == false) {
+                return 'done-task '
+            } else {
+                return ''
+            }
+        }
+    }
+}).mount('#app')
